62e42ec41099ec443b9373924372833e887dd12c
ANflo <75026239+AnFloGue@users.noreply.github.com>
2024-07-24 23:16:30 +0200

    03 Commit: Update base.html and index.html

--- Create_Task/app_01/urls.py
+++ Create_Task/app_01/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('task/', views.task, name='task'),
+    path('register/', views.register, name='register'),
+    path('create-task/', views.create_task, name='create-task'),
+    
+    path('reviews/', views.reviews, name='reviews'),
     path('member/', views.register, name='member'),
     path('cars/', views.cars, name='cars'),
-    path('register/', views.register, name='register'),
-    path('task/', views.task, name='task'),
-    path('reviews/', views.reviews, name='reviews'),
-    path('create-task/', views.create_task, name='create-task'),
 ]
