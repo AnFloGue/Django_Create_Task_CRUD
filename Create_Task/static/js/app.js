b580937df82d7f3f5b1c3e430ac4427babe41efa
ANflo <75026239+AnFloGue@users.noreply.github.com>
2024-07-27 17:48:31 +0200

    09 Commit: beautifying the base.html + crushing bugs

---
+++ Create_Task/static/js/app.js
@@ -0,0 +1,11 @@
+// Create_Task/static/js/app.js
+
+// Function to log a message to the console
+function logMessage() {
+    console.log("This is a message from the custom JavaScript file.");
+}
+
+//alert( 'Hello world');
+
+// Call the function to log the message
+logMessage();
